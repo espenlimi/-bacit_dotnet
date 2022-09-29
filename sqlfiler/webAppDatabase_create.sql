8a4bad1850d917d53385ace179afb4c2c9c4435e
Sarah Adina Kristiansen <sakristian@uia.no>
2022-09-29 15:25:20 +0200

    endret script, la til kobling mellom users og suggestions

--- sqlfiler/webAppDatabase_create.sql
+++ sqlfiler/webAppDatabase_create.sql
@@ -1,19 +1,30 @@
 USE webAppDatabase;
 
 CREATE OR REPLACE TABLE users (
-    id INT PRIMARY KEY,
+    userId INT PRIMARY KEY,
     name VARCHAR(20),
     email VARCHAR(30),
     phone VARCHAR(12)
 );
 
 CREATE OR REPLACE TABLE suggestions (
-    id int primary key auto_increment,
+    sugId int primary key auto_increment,
     title varchar(20),
     name varchar(20),
     team varchar(20),
     description varchar(500),
     timeStamp varchar(255)
+);
+
+CREATE OR REPLACE TABLE userSuggestions (
+    userId INT, 
+    sugId INT, 
+    CONSTRAINT userSugPK
+    PRIMARY KEY (userId, sugId),
+    CONSTRAINT userSugFK
+    FOREIGN KEY (userID) REFERENCES users(userId),
+    CONSTRAINT sugUserFk
+    FOREIGN KEY (sugId) REFERENCES suggestions(sugId)
 );
 
 
