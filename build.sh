d9a2cc0ed9441c96c86bb41482d1ee03be2de0bd
Rippjo <Kasperforum@hotmail.no>
2022-09-30 14:37:02 +0200

    Endringer på Build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 @echo off
 
-:: Kill running instance of container
+#Kill running instance of container
 docker kill webapp
 
-:: Builds image specified in Dockerfile
+#Builds image specified in Dockerfile
 docker image build -t webapp .
 
-:: Starts container with web application and maps port 80 (ext) to 80 (internal)
+#Starts container with web application and maps port 80 (ext) to 80 (internal)
 docker container run --rm -it -d --name webapp --publish 80:80 webapp
 
 echo.
 echo "Link: http://localhost:80/"
 echo.
 
-pause
