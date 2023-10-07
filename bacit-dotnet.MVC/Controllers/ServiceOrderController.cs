2fbd931ec00dc5cd3fd677a8d7d97958a763c5c9
johannesUIA <johannesma@student.uia.no>
2023-10-07 16:25:28 +0200

    Service Order side
    Simplifisert gamle ServiceOrder siden. Skal senere gjøres om til tabel i view

--- bacit-dotnet.MVC/Controllers/ServiceOrderController.cs
+++ bacit-dotnet.MVC/Controllers/ServiceOrderController.cs
@@ -1,38 +1,23 @@
 ﻿using bacit_dotnet.MVC.Models.ServiceOrder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
-namespace bacit_dotnet.MVC.Controllers
+namespace bacit_dotnet.MVC.Controllers;
+
+public class ServiceOrderController : Controller
 {
-    public class ServiceOrderController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Save(ServiceOrderViewModel model) {
+        if(ModelState.IsValid)
         {
-         //Oppprettet instanse 
-            var model = new ServiceOrderViewModel
-            {
-                BrukteTimer = 0,
-                Opprettelsesdato = new DateTime(2023, 9,27),
-                CustomerCity="Kristiansand",
-                Kundekommentar= "Hei og hå, jeg er en kundekommentar",
-                CustomerEmail="customer@thesystem.no",
-                CustomerName="Sattosk Rev",
-                CustomerStreet="Gata 13",
-                CustomerTelephoneNumber="1337",
-                CustomerZipcode="1234",
-                FutureMaintenance="Ingenting å bemerke",
-                ImageUrl="",
-                IsAdministrator=false,
-                OrderGroups = new List<ServiceOrderGroupModel> { 
-                    new ServiceOrderGroupModel {Name ="Mekanisk", Orders = new List<string>{"reperasjon"} },
-                
-                },
-                Mekanikker= "Espen",
-                MekanikkerKommentar= "ingen kommentar",
-                Ordrenummer= "pirepioj123åojå",
-                ServiceOrderId = 1
-            };
+            var s = "ineedabreakpoint";
 
-            return View(model);
         }
+        return View("Index", model);
     }
 }
--- bacit-dotnet.MVC/Models/ServiceOrder/ServiceOrderViewModel.cs
+++ bacit-dotnet.MVC/Models/ServiceOrder/ServiceOrderViewModel.cs
@@ -1,21 +1,9 @@
-﻿using System.Runtime.InteropServices.JavaScript;
-namespace bacit_dotnet.MVC.Models.ServiceOrder
+﻿namespace bacit_dotnet.MVC.Models.ServiceOrder
 {
     public class ServiceOrderViewModel
     {
-        public string Mekanikker { get; set; }
-        public bool IsAdministrator { get; set; }
-        public string Ordrenummer { get; set; }
-        public DateTime Opprettelsesdato { get; set; }
-        public decimal BrukteTimer { get; set; }
-        public string ImageUrl { get; set; }
-        public string MekanikkerKommentar { get; set; }
-        public string Kundekommentar { get; set; }
-
-        public string FutureMaintenance { get; set; }
         public int ServiceOrderId { get; set; }
-        public List<ServiceOrderGroupModel> OrderGroups { get; set; }
-
+        public DateTime CreatedDate { get; set; }
         public string CustomerName { get; set; }
         public string CustomerEmail { get; set; }
         public string CustomerStreet { get; set; }
@@ -23,13 +11,6 @@ namespace bacit_dotnet.MVC.Models.ServiceOrder
         public string CustomerZipcode { get; set; }
         public string CustomerTelephoneNumber { get; set; }
 
-    }
-  
-    public class ServiceOrderGroupModel
-    {
-        public string Name { get; set; }
-
-        public List<string> Orders { get; set; }
     }
 
 }
