ab941b8e3bdb2e8957f2431cb6229bbec2c236a5
TonyLe02 <tonynl@student.uia.no>
2023-10-08 12:37:43 +0200

    Update serviceskjema
    Fjernet unødvendig kode og forkortet css.

--- bacit-dotnet.MVC/wwwroot/js/serviceForm.js
+++
@@ -1,9 +0,0 @@
-// event to  "Send" button
-document.getElementById("sendButton").addEventListener("click", function () {
-
-    this.classList.add("animate-send");
-    
-    this.addEventListener("animationend", function () {
-        this.classList.remove("animate-send");
-    }, { once: true });
-});
