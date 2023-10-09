ee0d9b2619efcd91b60e272d58d0bda14659488b
johannestj <johannestj@student.uia.no>
2023-10-09 11:51:07 +0200

    Slettet ny serviceordre side, og oppdaterte serviceskjema

--- bacit-dotnet.MVC/Models/ServiceForm/ServiceFormViewModel.cs
+++ bacit-dotnet.MVC/Models/ServiceForm/ServiceFormViewModel.cs
@@ -1,6 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace bacit_dotnet.MVC.Models.ServiceForm;
 
 public class ServiceFormViewModel
 {
+    [Required]
+    public string Kunde { get; set; }
     
+    public DateTime MottattDato { get; set; }
+    
+    public string Adresse { get; set; }
+    
+    public string Email { get; set; }
+    
+    public int Ordrenummer { get; set; }
+    
+    public int telefon { get; set; }
+    
+    public string Produkttype { get; set; }
+    
+    public int Årsmodell { get; set; }
+    
+    public string Garanti { get; set; }
+    
+    public string Serienummer { get; set; }
+    
+    public string Avtale { get; set; }
+    
+    public string Reparasjonsbeskrivelse { get; set; }
+    
+    public string MedgåtteDeler { get; set; }
 }
