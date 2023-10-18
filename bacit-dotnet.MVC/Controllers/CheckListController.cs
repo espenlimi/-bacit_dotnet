e8d75aeb15a45a9d48914f98a2da78fff96cffb5
johannestj <johannestj@student.uia.no>
2023-10-18 13:25:12 +0200

    la til post funksjon til sjekkliste, og endret på småting utenom

--- bacit-dotnet.MVC/Controllers/CheckListController.cs
+++ bacit-dotnet.MVC/Controllers/CheckListController.cs
@@ -1,3 +1,4 @@
+using bacit_dotnet.MVC.Models.CheckList;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bacit_dotnet.MVC.Controllers;
@@ -8,5 +9,15 @@ public class CheckListController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Save(CheckListViewModel model) {
+        if(ModelState.IsValid)
+        {
+            var s = "ineedabreakpoint";
+
+        }
+        return View("Index", model);
     }
 }
