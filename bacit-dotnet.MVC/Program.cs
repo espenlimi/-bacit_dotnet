1da60b1ca5215a03844e95b2e8c8f13acb17dc99
johannesUIA <johannesma@student.uia.no>
2023-10-19 12:48:12 +0200

    lagt til kobling mot mock database
    endre i program.cs for

--- bacit-dotnet.MVC/Program.cs
+++ bacit-dotnet.MVC/Program.cs
@@ -9,7 +9,7 @@ public class Program
     static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        var ConnectionString = "Server=localhost;Port=3306;User ID=root;Password=123;Database=ReficioDB;CharSet=utf8";
+        var ConnectionString = "Server=localhost;Port=3306;User ID=root;Password=1;Database=ReficioDB;CharSet=utf8";
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
