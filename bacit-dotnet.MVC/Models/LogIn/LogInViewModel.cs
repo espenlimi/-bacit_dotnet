24317b189e3e3b83b1196df1f195afc24d7298d7
TonyLe02 <tonynl@student.uia.no>
2023-10-26 13:42:30 +0200

    LoginViewModel og oppdatert LogIn.cshtml

--- bacit-dotnet.MVC/Models/LogIn/LogInViewModel.cs
+++ bacit-dotnet.MVC/Models/LogIn/LogInViewModel.cs
@@ -1,6 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace bacit_dotnet.MVC.Models.LogIn;
 
 public class LogInViewModel
 {
+    [Required]
+    [EmailAddress]
+    public  string Email { get; set; }
     
+    [Required]
+    [EmailAddress]
+    public  string Password { get; set; }
 }
